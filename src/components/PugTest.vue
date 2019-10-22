7fd3556390db9cf4b7352bd76e3df407ea922011
Lanston Chandler <gcubewhiz@hotmail.com>
2019-10-22 12:56:01 -0700

    Pug migration

---
+++ src/components/PugTest.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+v-layout#wrapper
+    v-flex(xs12 ma-0)
+        v-card
+            v-card-text
+                p Welcome to the app!
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+    
+})
+</script>
+
+<style lang="stylus">
+
+</style>
